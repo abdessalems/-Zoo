1fb62a4e67ec7323ad8cf76f5416807d4cccbd56
Baganda49 <abdessalemsaa@gmail.com>
2024-06-19 16:45:24 +0200

    Initial commit

---
+++ src/main/java/com/example/zoo/ZooApplication.java
@@ -0,0 +1,13 @@
+package com.example.zoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zoo/ZooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
